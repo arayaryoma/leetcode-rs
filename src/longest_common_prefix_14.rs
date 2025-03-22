0680918235363bf8c2e5aafa83d3c7757f64bf0d
araya <araya@araya.dev>
2025-03-23 00:47:49 +0900

    Solve 14

---
+++ src/longest_common_prefix_14.rs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=14 lang=rust
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        strs.iter().fold(
+            strs.get(0).unwrap_or(&String::new()).clone(),
+            |prefix, s| {
+                prefix
+                    .chars()
+                    .zip(s.chars())
+                    .take_while(|(a, b)| a == b)
+                    .map(|(a, _)| a)
+                    .collect()
+            },
+        )
+    }
+}
+// @lc code=end
--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,4 @@ mod richest_customer_wealth;
 mod roman_to_integer;
 mod running_sum_of_1d_array;
 mod two_sum;
+mod longest_common_prefix_14;
